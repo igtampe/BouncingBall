2b1192905bc71a02cdf9a647cefc8beb358bd965
Ignacio Tampe <Igtampe@outlook.com>
2020-12-22 12:55:01 -0400

    Command line parameters and parsing. Icon.

--- BouncingBall/Program.cs
+++ BouncingBall/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using Igtampe.BasicRender;
 using Igtampe.BasicGraphics;
 
@@ -6,58 +7,138 @@ namespace Igtampe.BouncingBall {
     class Program {
 
         /// <summary>A 3x3 square-ish ball</summary>
-        public static string[] squareball3x3 = { " F1F1 ",
-                                                 "F1F1F1",
-                                                 " F1F1 " };
+        public static readonly string[] squareball3x3 = { " F1F1 ",
+                                                          "F1F1F1",
+                                                          " F1F1 " 
+        };
 
         /// <summary>Starts a bouncy ball</summary>
         /// <param name="args"></param>
         static void Main(string[] args) {
 
-            bool Cont = true;
             BasicBall Ball = new BasicBall {
                 XSpeed = 3,
                 BallGraphic = new BasicGraphic(squareball3x3,"SquareBall 3x3"),
                 Bounciness = 0.9,
                 Gravity = 0.5
-            };
+            }; //default settings
 
-            if(args.Length > 1) {
-                if(args[0].ToUpper().EndsWith(".DF")) { Ball.BallGraphic = BasicGraphic.LoadFromFile(args[0]); }
-                if(args[0].ToUpper().EndsWith(".HC")) { Ball.BallGraphic = HiColorGraphic.LoadFromFile(args[0]); }
+            bool ShowInfo = true;
+            int FPS = 30;
+            bool StopOnZero = false;
+
+            string ErrorString = "";
+
+            foreach(string arg in args) {
+                try {
+                    //split
+                    string[] SplitArg = arg.Split('=');
+                    if(SplitArg.Length < 2) { continue; }
+
+                    //Parse
+                    switch(SplitArg[0].ToUpper()) {
+                        case "/GRAPHIC":
+                            if(SplitArg[1].ToUpper().EndsWith(".DF")) { Ball.BallGraphic = BasicGraphic.LoadFromFile(SplitArg[1]); }
+                            if(SplitArg[1].ToUpper().EndsWith(".HC")) { Ball.BallGraphic = HiColorGraphic.LoadFromFile(SplitArg[1]); }
+                            break;
+                        case "/X":
+                            Ball.X = double.Parse(SplitArg[1]);
+                            break;
+                        case "/Y":
+                            Ball.Y = double.Parse(SplitArg[1]);
+                            break;
+                        case "/XSPEED":
+                            Ball.XSpeed = double.Parse(SplitArg[1]);
+                            break;
+                        case "/YSPEED":
+                            Ball.YSpeed = double.Parse(SplitArg[1]);
+                            break;
+                        case "/SPEED":
+                            //ok Speed is going to be formatted like: VEL:ANG (IE: 10:30 is 10 speed units at 30 degrees up
+                            string[] splitsplitarg = SplitArg[1].Split(':');
+
+                            double Vel = double.Parse(splitsplitarg[0]);
+                            double Ang = double.Parse(splitsplitarg[1])* (Math.PI / 180);
+
+                            Ball.XSpeed = Vel * Math.Cos(Ang);
+                            Ball.YSpeed = -1*Vel * Math.Sin(Ang); //negative since positive is *down*
+
+                            break;
+                        case "/BOUNCE":
+                        case "/BOUNCINESS":
+                            Ball.Bounciness = double.Parse(SplitArg[1]);
+                            break;
+                        case "/GRAV":
+                        case "/GRAVITY":
+                            Ball.Gravity = double.Parse(SplitArg[1]);
+                            break;
+                        case "/FPS":
+                            FPS = int.Parse(SplitArg[1]);
+                            break;
+                        case "/SHOWINFO":
+                            ShowInfo = bool.Parse(SplitArg[1]);
+                            break;
+                        case "/STOPONZERO":
+                            StopOnZero = bool.Parse(SplitArg[1]);
+                            break;
+                        default:
+                            break;
+                    }
+                } 
+                catch(FormatException) {ErrorString += "\nCould not parse parameter for " + arg;}
+                catch(FileNotFoundException) {ErrorString += "\nCould not find file for " + arg;}
+                catch(IndexOutOfRangeException) {ErrorString += "\nNo Parameter for " + arg;}
             }
 
+            if(!string.IsNullOrWhiteSpace(ErrorString)) {
+                RenderUtils.Echo("\nErrors occurred while parsing parameters:\n" + ErrorString + "\n\nPress CTRL+C to stop execution, or any other key to continue.");
+                RenderUtils.Pause();
+                Console.Clear();
+            }
 
+            BallRenderer(Ball,ShowInfo,FPS,StopOnZero);
+        }
+
+        public static void BallRenderer(BasicBall Ball, bool ShowInfo, int FPS, bool StopOnZero) {
+            bool Cont = true;
+
+            //Draw for first time
             Ball.BallGraphic.Draw(Convert.ToInt32(Ball.X),Convert.ToInt32(Ball.Y));
 
             string InfoString;
-            string DrawnInfoString="";
+            string DrawnInfoString = "";
 
             while(Cont) {
 
+                //Prepare infostring
                 InfoString = Ball.BallGraphic.Name + " (" + Ball.Width + "x" + Ball.Height + ")\n\n"
-                           + "Position  : " + Ball.X.ToString("0.00") + "," + Ball.Y.ToString("0.00") + "\n"
-                           + "Speed     : " + Ball.XSpeed.ToString("0.00") + "," + Ball.YSpeed.ToString("0.00") + "\n\n"
+                           + "Position  : " + Ball.X.ToString("0.00") + ", " + Ball.Y.ToString("0.00") + "\n"
+                           + "Speed     : " + Ball.XSpeed.ToString("0.00") + ", " + Ball.YSpeed.ToString("0.00") + "\n\n"
                            + "Gravity   : " + Ball.Gravity.ToString("0.00") + "\n"
                            + "Bounciness: " + Ball.Bounciness.ToString("0.00");
 
-                if(!InfoString.Equals(DrawnInfoString)) {
+                //Redraw the infostring if it is different.
+                if(!InfoString.Equals(DrawnInfoString) && ShowInfo) {
                     RenderUtils.SetPos(0,0);
                     RenderUtils.Echo(InfoString);
                     DrawnInfoString = InfoString;
                 }
 
-                try {Ball.Render();} catch(Exception) {}
-                
-                RenderUtils.Sleep(50);
+                //Just in case in a try-catch just in case because if the console shrinks while drawing it could cause an error
+                try { Ball.Render(); } catch(Exception) { } 
 
+                //Wait
+                RenderUtils.Sleep(1000/FPS);
+
+                //If the console has been resized, resize the ball's x and y limit
                 if(Ball.XLimit != Console.WindowWidth || Ball.YLimit != Console.WindowHeight) {
                     Ball.XLimit = Console.WindowWidth;
                     Ball.YLimit = Console.WindowHeight;
 
-                    Console.Clear();
+                    Console.Clear(); //Clear just to clean up any graphical glitches from the resize
                 }
 
+                //Read the console key
                 if(Console.KeyAvailable) {
                     switch(Console.ReadKey(true).Key) {
                         case ConsoleKey.UpArrow:
@@ -102,10 +183,15 @@ namespace Igtampe.BouncingBall {
                         case ConsoleKey.Escape:
                             //close
                             return;
+                        case ConsoleKey.I:
+                            ShowInfo = !ShowInfo;
+                            break;
                         default:
                             break;
                     }
                 }
+
+                if(StopOnZero && Convert.ToInt32(Ball.YSpeed) == 0 && Convert.ToInt32(Ball.XSpeed) == 0) { return; }
             }
         }
     }
